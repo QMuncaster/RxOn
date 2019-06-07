5399ceef7fd2e964160bf247392a6bc149de5df3
charlottestewart <costewart4@gmail.com>
2019-06-07 07:56:00 -0700

    Added Navbar component & styling

--- RxOn/imports/ui/App.js
+++ RxOn/imports/ui/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import PatientPage from './PatientPage.js';
-
+import Navbar from './Navbar'
 // App component - represents the whole app
 export default class App extends Component {
   render() {
     return (
+      <div className = "App">
+      <Navbar />
       <PatientPage />
+      </div>
     )
   }
 }
