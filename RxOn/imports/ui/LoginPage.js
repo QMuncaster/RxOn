d3109c4b80c5f8a31c6be48b16004b6c393f3169
charlottestewart <costewart4@gmail.com>
2019-06-19 14:10:39 -0700

    formatted login page and added styling

--- RxOn/imports/ui/LoginPage.js
+++ RxOn/imports/ui/LoginPage.js
@@ -8,15 +8,15 @@ import PharmacyProfile from "./PharmacyProfile";
 
 export default class LoginPage extends Component {
 
-    state ={
+    state = {
         userType: "patient"
     }
 
-    updateUserType =(event) =>{
-        this.setState({userType:event.target.value});
+    updateUserType = (event) => {
+        this.setState({ userType: event.target.value });
     }
 
-    createAccount= () => {
+    createAccount = () => {
         this.props.history.push('/signup');
         // Users.insert({
         //     userType,
@@ -26,8 +26,8 @@ export default class LoginPage extends Component {
         // });
     }
 
-    loginAccount=() =>{
-        if (this.state.userType==="patient") {
+    loginAccount = () => {
+        if (this.state.userType === "patient") {
             this.props.history.push('/patient/profile');
         } else {
             this.props.history.push('/pharmacy/profile');
@@ -38,31 +38,51 @@ export default class LoginPage extends Component {
     render() {
         return (
 
-            <form>
-                <input type="radio" name="user type " value="patient" defaultChecked onChange={this.updateUserType}/> Patient
-                <input type="radio" name="user type " value="pharmacy" onChange={this.updateUserType}/> Pharmacy
-                <p>
-                    <label className="email box">
-                        Email:
-                        <input className={"email content"}/>
-                    </label>
-                </p>
-                <p>
-                    <label className="password box">
-                        Password:
-                        <input type="password" className={"email content"}/>
-                    </label>
+            <div class="wrapper fadeInDown">
+                <div id="formContent">
 
-
-                </p>
-                <div className="buttons-section">
-                    <button  className="ui button login" onClick={this.loginAccount}> Login
-                    </button>
-                    <button  className="ui button create account" onClick= {this.createAccount}>
-                        Create Account
-                    </button>
+                <div class="fadeIn first">
+                    <img src="./styling/rxon.png" id="icon" alt="logo" width="42" height="42"/>
                 </div>
-            </form>
+
+                <form>
+                    <input type="radio" name="user type " value="patient" defaultChecked onChange={this.updateUserType} /> Patient
+                <input type="radio" name="user type " value="pharmacy" onChange={this.updateUserType} /> Pharmacy
+                {/* <p>
+                        <label className="email box">
+                            Email:
+                        <input class="fadeIn second" className={"email content"} />
+                        </label>
+                    </p>
+                    <p>
+                        <label className="password box">
+                            Password:
+                        <input class="fadeIn third" type="password" className={"email content"} />
+                        </label>
+                    </p>
+                    <div className="buttons-section">
+                        <button className="ui button login" onClick={this.loginAccount}> Login
+                    </button> */}
+
+                    <input className={"email content"} type="text" id="email" class="fadeIn second" name="email" placeholder="email"/>
+                    <input className={"email content"} type="text" id="password" class="fadeIn third" name="login" placeholder="password"/>
+                    <input onClick={this.loginAccount} type="submit" class="fadeIn fourth" value="Log In"/>
+
+
+
+                        <button id="buttonCreateAccount" class="fadeIn fourth"  className="ui button create account" onClick={this.createAccount}>
+                            Create Account
+                    </button>
+                    {/* </div> */}
+                </form>
+
+                <div id="formFooter">
+                    <a class="underlineHover" href="#">Forgot Password?</a>
+                </div>
+
+            </div>
+            </div>
+
 
         );
 
