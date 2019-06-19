12266804d538d7b71b147499c97f1138bead5615
charlottestewart <costewart4@gmail.com>
2019-06-19 14:57:24 -0700

    starting on create account page - very basic so far

--- RxOn/imports/ui/LoginPage.js
+++ RxOn/imports/ui/LoginPage.js
@@ -47,33 +47,14 @@ export default class LoginPage extends Component {
 
                 <form>
                     <input type="radio" name="user type " value="patient" defaultChecked onChange={this.updateUserType} /> Patient
-                <input type="radio" name="user type " value="pharmacy" onChange={this.updateUserType} /> Pharmacy
-                {/* <p>
-                        <label className="email box">
-                            Email:
-                        <input class="fadeIn second" className={"email content"} />
-                        </label>
-                    </p>
-                    <p>
-                        <label className="password box">
-                            Password:
-                        <input class="fadeIn third" type="password" className={"email content"} />
-                        </label>
-                    </p>
-                    <div className="buttons-section">
-                        <button className="ui button login" onClick={this.loginAccount}> Login
-                    </button> */}
-
+                    <input type="radio" name="user type " value="pharmacy" onChange={this.updateUserType} /> Pharmacy
                     <input className={"email content"} type="text" id="email" class="fadeIn second" name="email" placeholder="email"/>
                     <input className={"email content"} type="text" id="password" class="fadeIn third" name="login" placeholder="password"/>
                     <input onClick={this.loginAccount} type="submit" class="fadeIn fourth" value="Log In"/>
 
-
-
-                        <button id="buttonCreateAccount" class="fadeIn fourth"  className="ui button create account" onClick={this.createAccount}>
-                            Create Account
+                    <button id="buttonCreateAccount" class="fadeIn fourth"  className="ui button create account" onClick={this.createAccount}>
+                        Create Account
                     </button>
-                    {/* </div> */}
                 </form>
 
                 <div id="formFooter">
@@ -82,7 +63,6 @@ export default class LoginPage extends Component {
 
             </div>
             </div>
-
 
         );
 
