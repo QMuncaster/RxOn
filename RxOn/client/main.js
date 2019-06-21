28a4a6aa8462ec0b7f2258451ef446611cc413cb
omarkhs <omarkhsalem@gmail.com>
2019-06-21 02:19:25 -0700

    connect redux with react-router

--- RxOn/client/main.js
+++ RxOn/client/main.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import { renderRoutes } from './route.js';
-
+import store from "../imports/ui/store/store"
 Meteor.startup(() => {
-    render(renderRoutes(), document.getElementById('render-target'));
+    render(renderRoutes({store}), document.getElementById('render-target'));
 });
 
--- RxOn/client/route.js
+++ RxOn/client/route.js
@@ -1,33 +1,39 @@
-import React from 'react';
-import { Router, Switch } from 'react-router';
-import { BrowserRouter, Route } from 'react-router-dom'
+import React from "react";
+import { Switch } from "react-router";
+import { Provider } from "react-redux";
+import { BrowserRouter, Route } from "react-router-dom";
 // import createBrowserHistory from 'history/createBrowserHistory';
 
 // route components
-import Login from '../imports/ui/LoginPage';
-import Patient from '../imports/ui/PatientPage';
-import PatientProfile from '../imports/ui/PatientProfile';
+import Login from "../imports/ui/LoginPage";
+import Patient from "../imports/ui/PatientPage";
+import PatientProfile from "../imports/ui/PatientProfile";
 import Navbar from "../imports/ui/Navbar";
-import PharmacyProfile from '../imports/ui/PharmacyProfile';
-import SignupPage from '../imports/ui/SignupPage';
-import PatientPage from '../imports/ui/PatientPage';
-
+import PharmacyProfile from "../imports/ui/PharmacyProfile";
+import SignupPage from "../imports/ui/SignupPage";
+import PatientPage from "../imports/ui/PatientPage";
+import SimpleForm from "../imports/ui/SimpleForm";
+import App from "../imports/ui/App";
 // const browserHistory = createBrowserHistory();
 
-export const renderRoutes = () => (
-    <div>
-        <BrowserRouter>
-        <Navbar/>
-    
-        <Switch>
-            <Route exact path="/" component={Login}/>
-            <Route exact path="/createaccount" component={Login}/>
-            <Route exact path="/home" component={PatientPage}/>
-            <Route exact path="/patient" component={Patient}/>
-            <Route exact path="/patient/profile" component={PatientProfile}/>
-            <Route exact path="/pharmacy/profile" component ={PharmacyProfile}/>
-            <Route exact path="/signup" component = {SignupPage}/>
-        </Switch>
+export const renderRoutes = ({ store }) => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/app" component={App} />
+        <Route exact path="/createaccount" component={Login} />
+        <Route exact path="/home" component={PatientPage} />
+        <Route exact path="/patient" component={Patient} />
+        <Route exact path="/patient/profile" component={PatientProfile} />
+        <Route exact path="/pharmacy/profile" component={PharmacyProfile} />
+        <Route exact path="/signup" component={SignupPage} />
+      </Switch>
     </BrowserRouter>
-    </div>
+  </Provider>
 );
+
+// renderRoutes.propTypes = {
+//   store: PropTypes.object.isRequired
+// };
