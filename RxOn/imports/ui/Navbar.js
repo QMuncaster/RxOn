686c66c89824e899e4548c605407e89a117a2afc
QMuncaster <q.muncaster@gmail.com>
2019-06-20 22:44:41 -0700

    added additional user properties; name to navbar; logout functionality

--- RxOn/imports/ui/Navbar.js
+++ RxOn/imports/ui/Navbar.js
@@ -1,19 +1,51 @@
-import React from 'react'
+import React, { Component } from 'react';
 import { Link, NavLink } from 'react-router-dom'
 import './styling/Navbar.css'
+import { withTracker } from 'meteor/react-meteor-data';
 
-const Navbar = () => {
-    return (
-        <nav className = "navWrapper">
-            <div className = "container">
-                <ul className = "right">
-                 <li className = "li" ><Link to="/" style = {{color: 'white',textDecoration: 'none'}} > Logout </Link> </li>
-                    <li className = "li"> <Link to="/patient/profile" style = {{color: 'white',textDecoration: 'none'}} > Profile  </Link></li>
-                    <li className = "li"> <Link to="/home" style = {{color: 'white', textDecoration: 'none'}} > Home  </Link></li> 
-                </ul>
-            </div>
-        </nav>
-    )
+class Navbar extends Component {
+    
+    logout = () => {
+        Meteor.logout((error) => {
+            if (error) {alert(error)}
+            this.props.history.push('/');
+        })
+    }
+
+    render() {
+        return (
+            <nav className="navWrapper">
+                <div className="container">
+                    <ul className="right">
+                        {/* Only show logout button if someones logged i */}
+                        {/* This should be done for the profile tab as well but honestly its getting too complicated */}
+                        {/* Might be easier to have a "loggedInNavBar" and a "loggedOutNavBar" component */}
+                        {this.props.currentUser ?
+                            <li className="li" ><Link to="/" 
+                                onClick={this.logout}
+                                style={{ color: 'white', textDecoration: 'none' }} > Logout </Link> </li>
+                            : ''}
+                        <li className="li"> <Link to="/patient/profile" style={{ color: 'white', textDecoration: 'none' }} > Profile  </Link></li>
+                        <li className="li"> <Link to="/home" style={{ color: 'white', textDecoration: 'none' }} > Home  </Link></li>
+                        {/* The welcome message below for logged in users probably shouldn't be a list element either */}
+                        {this.props.currentUser ?
+                            <li className="li" style={{ color: 'white', textDecoration: 'none' }}>
+{                            console.log(this.props)}
+                                {this.props.currentUser.firstname + " " + this.props.currentUser.lastname}
+                            </li>
+                            : ''};
+
+                    </ul>
+                </div>
+            </nav>
+        )
+    }
 }
 
-export default Navbar
+export default withTracker(() => {
+    Meteor.subscribe('userData');
+
+    return {
+        currentUser: Meteor.user()
+    };
+})(Navbar);
--- RxOn/server/main.js
+++ RxOn/server/main.js
@@ -32,3 +32,19 @@ Accounts.validateNewUser(function (user) {
 Meteor.users.deny({
     update() { return true; }
 });
+
+// need to publish added account fields otherwise client cant get them
+// https://docs.meteor.com/api/accounts.html#Meteor-users
+Meteor.publish('userData', function () {
+    if (this.userId) {
+      return Meteor.users.find({ _id: this.userId }, {
+        // 0 = exclude from return document
+        // 1 = include in return document
+        fields: { firstname: 1, lastname: 1 } 
+      });
+    } else {
+      this.ready();
+    }
+});
+  
+  
