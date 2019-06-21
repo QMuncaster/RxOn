8250e7b2e83c827da699417cb8545d79b30e1aa6
omarkhs <omarkhsalem@gmail.com>
2019-06-21 03:34:36 -0700

    add in link to other login page

--- RxOn/imports/ui/Navbar.js
+++ RxOn/imports/ui/Navbar.js
@@ -1,19 +1,36 @@
-import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
-import './styling/Navbar.css'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import "./styling/Navbar.css";
 
 const Navbar = () => {
-    return (
-        <nav className = "navWrapper">
-            <div className = "container">
-                <ul className = "right">
-                 <li className = "li" ><Link to="/" style = {{color: 'white',textDecoration: 'none'}} > Logout </Link> </li>
-                    <li className = "li"> <Link to="/patient/profile" style = {{color: 'white',textDecoration: 'none'}} > Profile  </Link></li>
-                    <li className = "li"> <Link to="/home" style = {{color: 'white', textDecoration: 'none'}} > Home  </Link></li> 
-                </ul>
-            </div>
-        </nav>
-    )
-}
+  return (
+    <nav className="navWrapper">
+      <div className="container">
+        <ul className="right">
+          <li className="li">
+            <Link to="/login" style={{ color: "white", textDecoration: "none" }}>
+              Login2
+            </Link>
+          </li>
+          <li className="li">
+            <Link to="/" style={{ color: "white", textDecoration: "none" }}>
+              Logout
+            </Link>
+          </li>
+          <li className="li">
+            <Link to="/patient/profile" style={{ color: "white", textDecoration: "none" }}>
+              Profile
+            </Link>
+          </li>
+          <li className="li">
+            <Link to="/home" style={{ color: "white", textDecoration: "none" }}>
+              Home
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
