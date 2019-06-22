2fbc1ae4eaf2b478e2864eb3535ea0a0cc174b02
QMuncaster <QMuncaster@users.noreply.github.com>
2019-06-22 02:41:32 -0700

    Revert "merge accounts"

--- RxOn/imports/ui/PharmacyProfile.js
+++ RxOn/imports/ui/PharmacyProfile.js
@@ -1,51 +1,22 @@
 import React, {Component} from 'react';
-import {Prescriptions} from '../api/prescriptions';
-import PrescriptionForPharmacy from './PrescriptionForPharmacy';
-import {withTracker} from 'meteor/react-meteor-data';
-import './styling/PharmacyPrescription.css';
-import PharmarcySidebar from "./PharmarcySidebar";
 
-class PharmacytProfile extends Component {
+export default class PharmacytProfile extends Component {
 
-    renderPrescriptions(status) {
-        return this.props.prescriptions.map((px) => {
-                if (px.status === status) {
-                    console.log(px);
-                    return (<PrescriptionForPharmacy key={px._id} prescription={px}/>)
-                }
-            }
-        );
+    updateProfile() {
+        // Patients.update({
+        //     userType,
+        //     UserID,
+        //     date: new Date(),
+        // });
+        //TODO:
     }
 
 
     render() {
         return (
-            <div className="pharmacy-profile-page-heading" >
-                <PharmarcySidebar/>
-                <div className="pharmacy-profile-page">
-                    <div  className="Pharmacy-Headings">
-                        <h2 id="pharmHeading" className="prescription-header-individual"> Pending</h2>
-                        <div id="pendingBox">
-                            {this.renderPrescriptions("pending")}
-                        </div>
-                    </div>
-                    <div className="Pharmacy-Headings">
-                        <h2 id="pharmHeading" className="prescription-header-individual"> Filled</h2>
-                        <div id="filledBox">
-                            {this.renderPrescriptions("filled")}
-                        </div>
-                    </div>
-                </div>
+            <div className="pharmacy-profile-page">
+                <h2>Coming Soon</h2>
             </div>
         );
     }
 }
-
-//find out the prescription collection
-
-export default withTracker(() => {
-    return {
-        prescriptions: Prescriptions.find({}, {sort: {createdAt: -1}}).fetch(),
-    };
-})(PharmacytProfile);
-
--- RxOn/imports/ui/SignupPage.js
+++ RxOn/imports/ui/SignupPage.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import './styling/SignUpPage.css';
 
 export default class SignupPage extends Component {
 
@@ -15,68 +14,9 @@ export default class SignupPage extends Component {
 
     render() {
         return (
-            <div className="profile-page">
-                <h2>Create Account </h2>
-                <div className="names-section">
-                    <div className="image-section">
-                       
-                        {/*<a href="#">Change Image</a>*/}
-                    </div>
-                    <div className="name-section">
-                        <div className="gender-section">
-                            <label htmlFor="mr">Mr.</label>
-                            <input type="radio" id="mr" name="gender-radio-group" value="mr" defaultChecked />
-                            <label htmlFor="mrs">Mrs.</label>
-                            <input type="radio" id="mrs" name="gender-radio-group" value="mrs" />
-                            <label htmlFor="mx">Mx.</label>
-                            <input type="radio" id="mx" name="gender-radio-group" value="mx" />
-                        </div>
-                        <div className="firstname-section">
-                            <label>First Name:</label>
-                            <input type="text" id="firstname" />
-                        </div>
-                        <div className="lastname-section">
-                            <label>Last Name:</label>
-                            <input type="text" id="lastname" />
-                        </div>
-                    </div>
-                </div>
-
-                <div className="contact-section">
-                    <span>
-                        <label>Address:</label>
-                        <input type="text" id="address" />
-                    </span>
-
-                    <span>
-                        <label>City:</label>
-                        <input type="text" id="city" />
-
-                        <label>Country:</label>
-                        <input type="text" id="country" />
-                    </span>
-
-                    <span>
-                        <label>Phone Number:</label>
-                        <input type="text" id="phone-part1" />
-                        <input type="text" id="phone-part2" />
-                        <input type="text" id="phone-part3" />
-
-                        <label>Email:</label>
-                        <input type="email" id="email" />
-                    </span>
-                </div>
-
-                <div className="emergency-section">
-
-                </div>
-
-                <div className="action-buttons-section">
-                    <button className="ui button update-button" onClick={() => this.createProfile()}> Create Account </button>
-                </div>
-
+            <div className="signup-page">
+                <h2>Coming Soon</h2>
             </div>
-            
         );
     }
 }
