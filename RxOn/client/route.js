7aa9616d470f9d0f6b88bc956487bc14d81b6a23
QMuncaster <q.muncaster@gmail.com>
2019-07-05 15:19:36 -0700

    redirects through brower page access, but not through routing...

--- RxOn/client/route.js
+++ RxOn/client/route.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Switch } from "react-router";
 import { Provider } from "react-redux";
-import { BrowserRouter, Route } from "react-router-dom";
-// import createBrowserHistory from 'history/createBrowserHistory';
+import { Router, Route } from "react-router-dom";
+import { createBrowserHistory } from 'history';
 
 // route components
 import Login from "../imports/ui/LoginPage";
@@ -14,11 +14,27 @@ import SignupPage from "../imports/ui/SignupPage";
 import PatientPage from "../imports/ui/PatientPage";
 import Login2 from "../imports/ui/login_components/Login";
 import App from "../imports/ui/prescription_modal/App"
-// const browserHistory = createBrowserHistory();
+const history = createBrowserHistory();
+
+const unauthenticated = ['/', '/signup'];
+
+export const handleAuthChange = (authenticated) => {
+  const path = history.location.pathname;
+  const requiresAuth = !unauthenticated.includes(path);
+  if (requiresAuth && !authenticated) {
+    history.replace('/login');
+  }
+};
+
+// this code will autorun whenever theres a change to user id
+Tracker.autorun(() => {
+  const authenticated = !!Meteor.userId();
+  handleAuthChange(authenticated);
+});
 
 export const renderRoutes = ({ store }) => (
   <Provider store={store}>
-    <BrowserRouter>
+    <Router history = {history}>
       <Navbar />
       <Switch>
         <Route exact path="/" component={Login} />
@@ -30,11 +46,8 @@ export const renderRoutes = ({ store }) => (
         <Route exact path="/patient/profile" component={PatientProfile} />
         <Route exact path="/pharmacy/profile" component={PharmacyProfile} />
         <Route exact path="/signup" component={SignupPage} />
+        {/* <PrivateRoute authed={this.state.authed} exact path="/patient/profile" component={PatientProfile} /> */}
       </Switch>
-    </BrowserRouter>
+    </Router>
   </Provider>
 );
-
-// renderRoutes.propTypes = {
-//   store: PropTypes.object.isRequired
-// };
