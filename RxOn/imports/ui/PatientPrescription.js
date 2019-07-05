bae9d9cc522a87a9884711d65a4ec0c29000ca80
omarkhs <omarkhsalem@gmail.com>
2019-07-05 16:20:41 -0700

    add request date column

--- RxOn/imports/ui/PatientPrescription.js
+++ RxOn/imports/ui/PatientPrescription.js
@@ -8,17 +8,25 @@ export default class PatientPrescription extends Component {
     Prescriptions.remove(this.props.prescription._id);
   }
 
+  // editPrescription() {
+
+  // }
+
   render() {
     return (
      <tr className="tr">
         <td className="th">{this.props.prescription.rxName}</td>
         <td className="th">{this.props.prescription.rxStrength}</td>
         <td className="th">{this.props.prescription.rxDose}</td>
+        <td className="th">{this.props.prescription.createdAt.toLocaleDateString("en-US")}</td>
         <td className="th">{this.props.prescription.status}</td>
         <td className="delete">
           {/* TODO: add functionality so canceling can only be done for pending prescriptions */}
           <button className="delete" onClick={this.cancelPrescription.bind(this)}>
             Cancel
+          </button>
+          <button className="edit">
+            Edit
           </button>
         </td>
       </tr>
