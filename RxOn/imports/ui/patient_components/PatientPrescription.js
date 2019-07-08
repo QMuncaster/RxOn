a88914b47cded17c95626a328b0899038a15a0e8
QMuncaster <q.muncaster@gmail.com>
2019-07-08 13:29:35 -0700

    changed time to date string

--- RxOn/imports/ui/patient_components/PatientPrescription.js
+++ RxOn/imports/ui/patient_components/PatientPrescription.js
@@ -18,7 +18,7 @@ export default class PatientPrescription extends Component {
         <td className="th">{this.props.prescription.rxName}</td>
         <td className="th">{this.props.prescription.rxStrength}</td>
         <td className="th">{this.props.prescription.rxDose}</td>
-        <td className="th">{this.props.prescription.createdAt? this.props.prescription.createdAt.toString() : ''}</td>
+        <td className="th">{this.props.prescription.createdAt? this.props.prescription.createdAt.toDateString() : ''}</td>
         <td className="th">{this.props.prescription.status}</td>
         <td className="delete">
           <button className="delete" onClick={this.cancelPrescription.bind(this)}>
