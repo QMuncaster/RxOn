7c900187b2956c34658935260db7dfe2a10a8d5a
mandy169 <mandysu169@gmail.com>
2019-07-12 16:15:06 -0700

    Edit prescription functionality worked- Issue#19 validation of prescriptions- Issue#32 server-side validation- Issue#25

--- RxOn/imports/collections/server/prescription_methods.js
+++ RxOn/imports/collections/server/prescription_methods.js
@@ -4,8 +4,17 @@ import { Prescriptions } from "../prescriptions.js";
 Meteor.methods({
     'prescriptions.insert'(name, strength, dose) {
       if (!this.userId) { throw new Meteor.Error('not-authorized'); }
-  
+
       // TODO: validation of arguments before inserting
+      // https://docs.meteor.com/api/check.html
+      // https://stackoverflow.com/questions/37237023/meteor-check-vs-new-simpleschema-for-verifying-publish-arguments
+      // it seems like schema already help with the validation.
+      // base on the second link, we can also use schema.validate to do it but check package is well enough.
+
+      check(name,String);
+      check (strength, String);
+      check(dose, String);
+
       Prescriptions.insert({
         patientId: this.userId,
         rxName: name,
@@ -13,24 +22,43 @@ Meteor.methods({
         rxDose: dose,
       });
     },
-  
-    'prescriptions.remove'(id) {
+
+    'prescriptions.remove'(id,status) {
       if (!this.userId) { throw new Meteor.Error('not-authorized'); }
-  
-      // TODO:
-      // should only be able to cancel if userId matches prescription's userId
-      // should only be able to cancel prescription with status = pending
-      Prescriptions.remove(id);
+
+        // TODO:
+        // should only be able to cancel if userId matches prescription's userId
+        // should only be able to cancel prescription with status = pending
+      if(this.userId !== id || status !== "pending") {
+          { throw new Meteor.Error('you are not able to cancel')}
+
+      }
+        Prescriptions.remove(id);
+
     },
-  
+
     // only for pharmacist users
     'prescriptions.fill'(id) {
       var loggedInUser = Meteor.user()
       if (!loggedInUser || !Roles.userIsInRole(loggedInUser, 'admin')) {
         throw new Meteor.Error(403, "Access denied")
       }
-  
+
       Prescriptions.update({_id: id}, {$set: {status: "filled"}})
-    }
+    },
+
+    'prescriptions.edit'(id,name,strength,dose){
+
+        //because we have admin acount here, so I am not checking the loggedInUser === the id
+        // we passed in, because I think the admin account has the right to edit all the prescription?
+        // var loggedInUser = Meteor.user();
+        // if (loggedInUser !== id) {
+        //     throw new Meteor.Error(403, "Access denied")
+        // }
+        check(name,String);
+        check (strength, String);
+        check(dose, String);
+        Prescriptions.update({_id: id}, {$set: {rxName: name, rxStrength:strength, rxDose: dose}})
+    },
+
   });
-  
--- RxOn/tests/main.js
+++ RxOn/tests/main.js
@@ -18,3 +18,6 @@ describe("RxOn", function () {
     });
   }
 });
+
+
+
