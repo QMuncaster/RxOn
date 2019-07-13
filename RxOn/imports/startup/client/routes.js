7882799e66522bf4c414a6e2f2817e701b01ecc9
omarkhs <omarkhsalem@gmail.com>
2019-07-12 18:52:13 -0700

    add routes to new components

--- RxOn/imports/startup/client/routes.js
+++ RxOn/imports/startup/client/routes.js
@@ -1,19 +1,23 @@
-import React from "react";
-import { Switch } from "react-router";
-import { Provider } from "react-redux";
-import { Router, Route, Redirect } from "react-router-dom";
+import React from 'react';
+import { Switch } from 'react-router';
+import { Provider } from 'react-redux';
+import { Router, Route, Redirect } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 
 // route components
-import Navbar from "../../ui/Navbar";
+import Navbar from '../../ui/Navbar';
+import Header from '../../ui/layouts/Header.js';
+import PatientProfile from '../../ui/patient_components/PatientProfile';
+import PatientPage from '../../ui/patient_components/PatientPage';
 
-import PatientProfile from "../../ui/patient_components/PatientProfile";
-import PatientPage from "../../ui/patient_components/PatientPage";
+import PharmacyProfile from '../../ui/pharmacist_components/PharmacyProfile';
+import PrescriptionTable from '../../ui/pharmacist_components/PrescriptionTable';
+import SignupPage from '../../ui/SignupPage';
+import Login from '../../ui/login_components/Login';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import themes from '../../ui/themes/index';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
-import PharmacyProfile from "../../ui/pharmacist_components/PharmacyProfile";
-
-import SignupPage from "../../ui/SignupPage";
-import Login from "../../ui/login_components/Login";
 const history = createBrowserHistory();
 
 // HANDLE AUTH FOR ROUTING
@@ -21,42 +25,56 @@ const history = createBrowserHistory();
 // https://tylermcginnis.com/react-router-protected-routes-authentication/
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route {...rest} render={(props) => (
-    // props is match, location, history, if we want to use in Component/Redirect
-    Meteor.userId() ?
-      <Component /> :
-      <Redirect to='/login' />
-  )
-  } />
-)
+   <Route
+      {...rest}
+      render={props =>
+         // props is match, location, history, if we want to use in Component/Redirect
+         Meteor.userId() ? <Component /> : <Redirect to='/login' />
+      }
+   />
+);
+
+const theme = createMuiTheme({
+   ...themes.default,
+   typography: {
+      useNextVariants: true,
+   },
+});
 
 export const renderRoutes = ({ store }) => (
-  <Provider store={store}>
-    <Router history={history}>
-      <Navbar />
-      <Switch>
-        <PrivateRoute exact path="/" component={PatientPage} />
-        <PrivateRoute exact path="/home" component={PatientPage} />
-        <PrivateRoute exact path="/pharmacy/profile" component={PharmacyProfile} />
-        <PrivateRoute exact path="/patient/profile" component={PatientProfile} />
-
-        <Route exact path="/logout" render={() => {
-          Meteor.logout((error) => {
-            if (error) { alert(error) }
-            history.push("/login");
-          });
-          return <Login />
-        }} />
-
-        <Route exact path="/login" render={() => (
-          Meteor.userId() ?
-            <Redirect to='/home' /> :
-            <Login />
-        )} />
-
-        <Route exact path="/signup" component={SignupPage} />
-        <Route render={() => (<h1>404 page not found</h1>)} />
-      </Switch>
-    </Router>
-  </Provider>
+   <MuiThemeProvider theme={theme}>
+      <Provider store={store}>
+         <Router history={history}>
+            <CssBaseline />
+            <Header />
+            <Switch>
+               <PrivateRoute exact path='/' component={PatientPage} />
+               <PrivateRoute exact path='/home' component={PatientPage} />
+               <PrivateRoute exact path='/pharmacy/profile' component={PharmacyProfile} />
+               <PrivateRoute exact path='/pharmacy/table' component={PrescriptionTable} />
+               <PrivateRoute exact path='/patient/profile' component={PatientProfile} />
+               <Route
+                  exact
+                  path='/logout'
+                  render={() => {
+                     Meteor.logout(error => {
+                        if (error) {
+                           alert(error);
+                        }
+                        history.push('/login');
+                     });
+                     return <Login />;
+                  }}
+               />
+               <Route
+                  exact
+                  path='/login'
+                  render={() => (Meteor.userId() ? <Redirect to='/home' /> : <Login />)}
+               />
+               <Route exact path='/signup' component={SignupPage} />
+               <Route render={() => <h1>404 page not found</h1>} />
+            </Switch>
+         </Router>
+      </Provider>
+   </MuiThemeProvider>
 );
