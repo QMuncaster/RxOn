0ffe41581f0770e5aeeba28a426e5f7e48d5c309
omarkhs <omarkhsalem@gmail.com>
2019-07-12 18:48:50 -0700

    font injection

--- RxOn/client/main.js
+++ RxOn/client/main.js
@@ -1,18 +1,17 @@
-import { Meteor } from 'meteor/meteor';
-import { render } from 'react-dom';
-import { renderRoutes } from '../imports/startup/client/routes.js';
-import store from "../imports/ui/store/store"
-import WebFont from 'webfontloader';
+import { Meteor } from "meteor/meteor";
+import { render } from "react-dom";
+import { renderRoutes } from "../imports/startup/client/routes.js";
+import store from "../imports/ui/store/store";
+import WebFont from "webfontloader";
 // this file is the main client-side entry point
 
-// This is needed to use Roboto font used by Material UI
-WebFont.load({
-  google: {
-    families: ['Roboto:300,400,500']
-  }
-});
-
 Meteor.startup(() => {
-    render(renderRoutes({store}), document.getElementById('render-target'));
+  // This injects Roboto fonts used by Material UI
+  WebFont.load({
+    google: {
+      families: ["Roboto:300,400,500"]
+    }
+  });
+  render(
+    renderRoutes({ store }), document.getElementById("render-target"));
 });
-
