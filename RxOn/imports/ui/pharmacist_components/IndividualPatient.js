931fb59ea85f17374444d854d64cdeb95dc33088
charlottestewart <costewart4@gmail.com>
2019-07-16 15:24:23 -0700

    more changes made

--- RxOn/imports/ui/pharmacist_components/IndividualPatient.js
+++ RxOn/imports/ui/pharmacist_components/IndividualPatient.js
@@ -3,13 +3,14 @@ import "../styling/PharmacyPrescription";
 
 
 export default class IndividualPatient extends Component {
-    
+
     render() {
 
         return (
             <div id="rxForm" className="admin-prescription-form ">
     
                     <ul> 
+                        <li className="listItem"> {this.props.user.email}  </li> 
                         <li className="listItem"> {this.props.user.lastname}, {this.props.user.firstname}  </li>     
                     </ul>
             
