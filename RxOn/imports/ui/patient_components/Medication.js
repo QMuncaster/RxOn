722f2dc4cc9a190b318141fd6080de1fd89d02b5
omarkhs <omarkhsalem@gmail.com>
2019-07-19 23:53:46 -0700

    add actions to medication component

--- RxOn/imports/ui/patient_components/Medication.js
+++ RxOn/imports/ui/patient_components/Medication.js
@@ -1,63 +1,66 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/styles";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import ListItemAvatar from "@material-ui/core/ListItemAvatar";
-import Avatar from "@material-ui/core/Avatar";
-import Typography from "@material-ui/core/Typography";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import CancelIcon from '@material-ui/icons/Cancel';
+import EditIcon from '@material-ui/icons/Edit';
+import EditAction from './MedicationEdit';
 
-const styles = {
-  root: {
-    maxWidth: 360
-  },
-  inline: {
-    display: "inline"
-  },
-  avatar: {
-    margin: 10
-  }
-};
+const styles = theme => ({
+    root: {
+        maxWidth: 360,
+    },
+    inline: {
+        display: 'inline',
+    },
+    avatar: {
+        margin: 10,
+    },
+});
 
 function Medication(props) {
-  const { classes, ContainerProps } = props;
-  const name = ContainerProps.rxName + " " + ContainerProps.rxStrength;
-  const directions = ContainerProps.rxDose;
-  return (
-    <List className={classes.root}>
-      <ListItem alignItems="flex-start" divider={true}>
-        <ListItemAvatar>
-          <Avatar>PH</Avatar>
-        </ListItemAvatar>
-        <ListItemText
-          primary={name}
-          secondary={
-            <React.Fragment>
-              <Typography
-                component="span"
-                className={classes.inline}
-                color="textSecondary"
-              >
-                Directions:{" "}
-              </Typography>
-              <Typography
-                component="span"
-                className={classes.inline}
-                color="textPrimary"
-              >
-                {directions}
-              </Typography>
-            </React.Fragment>
-          }
-        />
-      </ListItem>
-    </List>
-  );
+    const { classes, ContainerProps } = props;
+    const name = ContainerProps.rxName + ' ' + ContainerProps.rxStrength;
+    const directions = ContainerProps.rxDose;
+    return (
+        <List>
+            <ListItem alignItems="flex-start" divider={true}>
+                <ListItemAvatar>
+                    <Avatar>PH</Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                    primary={name}
+                    secondary={
+                        <React.Fragment>
+                            <Typography component="span" className={classes.inline} color="textSecondary">
+                                Directions:{' '}
+                            </Typography>
+                            <Typography component="span" className={classes.inline} color="textPrimary">
+                                {directions}
+                            </Typography>
+                        </React.Fragment>
+                    }
+                />
+                <ListItemSecondaryAction>
+                    <EditAction ContainerProps={ContainerProps} />
+                    <IconButton edge="end" aria-label="Cancel" color="secondary">
+                        <CancelIcon />
+                    </IconButton>
+                </ListItemSecondaryAction>
+            </ListItem>
+        </List>
+    );
 }
 
 Medication.propTypes = {
-  classes: PropTypes.object.isRequired
+    classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(Medication);
