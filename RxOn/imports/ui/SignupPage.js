069acc0ecdfba24c85b9dc7c5ac30d22ba3c6286
charlottestewart <costewart4@gmail.com>
2019-08-03 14:25:37 -0700

    fixing sign up page

--- RxOn/imports/ui/SignupPage.js
+++ RxOn/imports/ui/SignupPage.js
@@ -1,9 +1,9 @@
-
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 
 export default function AddPatient() {
@@ -11,15 +11,13 @@ export default function AddPatient() {
     const [values, setValues] = useState({
         firstname: '',
         lastname: '',
-        address: '', 
-       
+        address: '',
     });
 
     const handleChange = valName => event => {
         setValues({ ...values, [valName]: event.target.value });
     };
 
-   
     function handleSave() {
         Meteor.call(
             Accounts.createUser({
@@ -39,95 +37,94 @@ export default function AddPatient() {
         );
     }
 
-    
-  return (
-    <React.Fragment>
-      <div style={{ paddingLeft: 150, paddingRight: 150, paddingTop: 75 }}>
-      <Typography variant="h2" gutterBottom>
-        SignUp Page
-      </Typography>
-      
-      <Grid container spacing={3}>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="firstname"
-            name="firstname"
-            label="First Name"
-            fullWidth
-            autoComplete="fname"
-            //onChange={handleChange('firstname')}
-          /> 
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="lastname"
-            name="lastname"
-            label="Last Name"
-            fullWidth
-            autoComplete="lastname"
-            //onChange={handleChange('lastname')}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            required
-            id="address"
-            name="address"
-            label="Address"
-            fullWidth
-            autoComplete="address"
-          />
-        </Grid>
-        
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="email"
-            name="email"
-            label="Email"
-            fullWidth
-            autoComplete="email"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField id="state" name="state" label="State/Province/Region" fullWidth />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="password"
-            name="password"
-            label="Password"
-            fullWidth
-            autoComplete="password"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            id="country"
-            name="country"
-            label="Country"
-            fullWidth
-            autoComplete="billing country"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          {/* <FormControlLabel
-            control={<Checkbox color="secondary" name="saveAddress" value="yes" />}
-            label="Use this address for payment details"
-          /> */}
+    return (
+        <React.Fragment>
+            <div>
+                <Link to="/" >
+                    LOGIN
+                        </Link>
+            </div>
+            <div style={{ paddingLeft: 150, paddingRight: 150, paddingTop: 75 }}>
 
-        <div style={{ paddingTop: 20 }}>
-        <Button onClick={handleSave} color="primary">
-                        Sign Up
-         </Button>
-          </div>
+                <Typography variant="h2" gutterBottom>
+                    SignUp Page
+                </Typography>
 
-        </Grid>
-      </Grid>
-      </div>
-    </React.Fragment>
-  );
+                <Grid container spacing={3}>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            required
+                            id="firstname"
+                            name="firstname"
+                            label="First Name"
+                            fullWidth
+                            autoComplete="fname"
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            required
+                            id="lastname"
+                            name="lastname"
+                            label="Last Name"
+                            fullWidth
+                            autoComplete="lastname"
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            required
+                            id="address"
+                            name="address"
+                            label="Address"
+                            fullWidth
+                            autoComplete="address"
+                        />
+                    </Grid>
+
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            required
+                            id="email"
+                            name="email"
+                            label="Email"
+                            fullWidth
+                            autoComplete="email"
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <TextField id="state" name="state" label="State/Province/Region" fullWidth />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            required
+                            id="password"
+                            name="password"
+                            label="Password"
+                            fullWidth
+                            autoComplete="password"
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            id="country"
+                            name="country"
+                            label="Country"
+                            fullWidth
+                            autoComplete="billing country"
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                 
+                        <div style={{ paddingTop: 20 }}>
+                            <Button onClick={handleSave} color="primary">
+                                Sign Up
+                            </Button>
+                        </div>
+
+                    </Grid>
+                </Grid>
+            </div>
+        </React.Fragment>
+    );
 }
