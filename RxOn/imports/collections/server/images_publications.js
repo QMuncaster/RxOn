a9f732b9a586b087c10ed173817b535886100fce
omarkhs <30681552+omarkhs@users.noreply.github.com>
2019-08-06 00:24:40 -0700

    restrict image publications

--- RxOn/imports/collections/server/images_publications.js
+++ RxOn/imports/collections/server/images_publications.js
@@ -1,6 +1,15 @@
 import { Images } from './images';
 import { Meteor } from 'meteor/meteor';
 
-Meteor.publish('files.all', function() {
-    return Images.find().cursor;
+Meteor.publish('images', function() {
+   let selector = { 'meta.patientId': Meteor.userId() };
+   let options = {
+      fields: {},
+   };
+   // Note: this find is MeteorFiles find method not MongoCollection find
+   return Images.find(selector, options).cursor;
+});
+
+Meteor.publish('images.all', function() {
+   return Images.find().cursor;
 });
