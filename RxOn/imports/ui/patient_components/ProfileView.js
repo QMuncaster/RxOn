a23ea1b01a95829d8c8f06dec591fd413b402283
QMuncaster <q.muncaster@gmail.com>
2019-08-06 22:40:26 -0700

    made fields editable on client side

--- RxOn/imports/ui/patient_components/ProfileView.js
+++ RxOn/imports/ui/patient_components/ProfileView.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
-import MenuItem from '@material-ui/core/MenuItem';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
+import MenuItem from '@material-ui/core/MenuItem';
 
 const styles = theme => ({
     textField: {
@@ -16,13 +16,52 @@ const styles = theme => ({
       width: 200,
     },
   });
-  
+
+const sexes = [{ label: 'Male', value: 'Male' }, { label: 'Female', value: 'Female' }, { label: 'Other', value: 'Other' }];
 
 class ProfileView extends React.Component {
+    state = {
+        fieldsDisabled: true,
+
+        firstName: '',
+        firstNameError: false,
+        firstNameErrorText: '',
+
+        lastName: '',
+        lastNameError: false,
+        lastNameErrorText: '',
+
+        sex: '',
+        sexError: false,
+        sexErrorText: '',
+
+        address: '',
+        addressError: false,
+        addressErrorText: '',
+    }
 
     handleEditButtonClick = () => {
-        // this.setState({ [name]: event.target.value });
-      };    
+        this.setState({firstName: this.props.firstName})
+        this.setState({lastName: this.props.lastName})
+        this.setState({sex: this.props.sex})
+        this.setState({address: this.props.address})
+        this.setState({fieldsDisabled: false});
+    };    
+
+    handleChange = (name) => event => {
+        this.setState({ [name]: event.target.value });
+    };
+
+    // function returns either the Meteor user info if not editing
+    // or the text from the state when editing
+    getValue = (field) => {
+        if (!this.state.fieldsDisabled) {
+            return this.state[field];
+        }
+        // '' is a hack to avoid warning:
+        // A component is changing a controlled input of type text to be uncontrolled.
+        return this.props[field] || '';
+    }
 
     render() {
         const { classes } = this.props;
@@ -34,41 +73,56 @@ class ProfileView extends React.Component {
                         required
                         id="firstName"
                         label="First Name"
-                        // '' is a hack to avoid uncontrolled component warning
-                        value={this.props.firstName || ''}
+                        value={this.getValue('firstName')}
+                        onChange={this.handleChange('firstName')}
                         className={classes.textField}
                         margin="normal"
-                        disabled={true}
+                        disabled={this.state.fieldsDisabled}
                     />
 
                     <TextField
                         required
                         id="lastName"
                         label="Last Name"
-                        value={this.props.lastName || ''}
+                        value={this.getValue('lastName')}
+                        onChange={this.handleChange('lastName')}
                         className={classes.textField}
                         margin="normal"
-                        disabled={true}
+                        disabled={this.state.fieldsDisabled}
                     />
 
                     <TextField
                         required
-                        id="sex"
+                        id="standard-select-sex"
+                        select
                         label="Sex"
-                        value={this.props.sex || ''}
                         className={classes.textField}
+                        value={this.getValue('sex')}
+                        onChange={this.handleChange('sex')}
+                        SelectProps={{
+                            MenuProps: {
+                                className: classes.menu,
+                            },
+                        }}
                         margin="normal"
-                        disabled={true}
-                    />
+                        disabled={this.state.fieldsDisabled}
+                    >
+                        {sexes.map(option => (
+                            <MenuItem key={option.value} value={option.value}>
+                                {option.label}
+                            </MenuItem>
+                        ))}
+                    </TextField>
 
                     <TextField
                         required
                         id="address"
                         label="Address"
-                        value={this.props.address || ''}
+                        value={this.getValue('address')}
+                        onChange={this.handleChange('address')}
                         className={classes.textField}
                         margin="normal"
-                        disabled={true}
+                        disabled={this.state.fieldsDisabled}
                     />
                 </div>
 
