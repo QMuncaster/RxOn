9194d8091da4f1dde5335fab0592e6e075be82da
omarkhs <30681552+omarkhs@users.noreply.github.com>
2019-08-07 05:22:05 -0700

    remove rename functionality

--- RxOn/imports/ui/patient_components/image_upload/FileIndividualFile.js
+++ RxOn/imports/ui/patient_components/image_upload/FileIndividualFile.js
@@ -4,38 +4,19 @@ import PropTypes from 'prop-types';
 class IndividualFile extends Component {
     constructor(props) {
         super(props);
-
         this.state = {};
-
         this.removeFile = this.removeFile.bind(this);
-        this.renameFile = this.renameFile.bind(this);
     }
 
     removeFile() {
         let conf = confirm('Are you sure you want to delete the file?') || false;
         if (conf == true) {
-            Meteor.call('images.removeFile', this.props.fileId, function(err, res) {
+            Meteor.call('images.RemoveFile', this.props.fileId, function(err, res) {
                 if (err) console.log(err);
             });
         }
     }
 
-    renameFile() {
-        let validName = /[^a-zA-Z0-9 \.:\+()\-_%!&]/gi;
-        let prompt = window.prompt('New file name?', this.props.fileName);
-
-        // Replace any non valid characters, also do this on the server
-        if (prompt) {
-            prompt = prompt.replace(validName, '-');
-            prompt.trim();
-        }
-
-        if (!_.isEmpty(prompt)) {
-            Meteor.call('RenameFile', this.props.fileId, prompt, function(err, res) {
-                if (err) console.log(err);
-            });
-        }
-    }
 
     render() {
         return (
@@ -48,14 +29,6 @@ class IndividualFile extends Component {
                 </div>
 
                 <div className="row">
-                    <div className="col-md-3">
-                        <button
-                            onClick={this.renameFile}
-                            className="btn btn-outline btn-primary btn-sm"
-                        >
-                            Rename
-                        </button>
-                    </div>
 
                     <div className="col-md-3">
                         <a
