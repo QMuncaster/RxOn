dbddc619b8ec66f6fca6bb7083ec3c139d909887
omarkhs <30681552+omarkhs@users.noreply.github.com>
2019-08-07 23:00:19 -0700

    remove reactive variable

--- RxOn/imports/ui/layouts/Header.js
+++ RxOn/imports/ui/layouts/Header.js
@@ -6,17 +6,17 @@ import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import ProfileMenu from './ProfileMenu';
-import { withTracker } from 'meteor/react-meteor-data';
-import { Tracker } from 'meteor/tracker';
+// import { withTracker } from 'meteor/react-meteor-data';
+// import { Tracker } from 'meteor/tracker';
 import { withRouter } from 'react-router-dom';
 
 // reactive variable that we can override for logging out
 // if just use this.props.currentUser, cannot set property to false before waiting for server response
 // so the NavBar would take a couple seconds to disappear after logging out
-let loggedIn = !!Meteor.user();
-Tracker.autorun(() => {
-    loggedIn = !!Meteor.user();
-});
+// let loggedIn = !!Meteor.user();
+// Tracker.autorun(() => {
+//     loggedIn = !!Meteor.user();
+// });
 
 const styles = {
     grow: {
@@ -31,7 +31,7 @@ class Header extends Component {
 
     render() {
         const { classes } = this.props;
-        if (loggedIn) {
+        //if (loggedIn) {
             return (
                 <div className={classes.grow}>
                     <AppBar position="static">
@@ -40,24 +40,24 @@ class Header extends Component {
                                 <MenuIcon />
                             </IconButton>
                             <Typography variant="h5" color="inherit" className={classes.grow}>
-                                Welcome {this.props.currentUser.firstname}
+                                Welcome
                             </Typography>
                             <ProfileMenu />
                         </Toolbar>
                     </AppBar>
                 </div>
             );
-        } else {
-            return null;
-        }
+        // } else {
+        //     return null;
+        // }
     }
 }
 
 const StyledHeader = withStyles(styles)(Header);
-
-export default withTracker(() => {
-    Meteor.subscribe('userData');
-    return {
-        currentUser: Meteor.user(),
-    };
-})(withRouter(StyledHeader));
+export default withRouter(StyledHeader);
+// export default withTracker(() => {
+//     Meteor.subscribe('userData');
+//     return {
+//         currentUser: Meteor.user(),
+//     };
+// })(withRouter(StyledHeader));
