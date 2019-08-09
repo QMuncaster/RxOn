ad720d7cd16c76ccafeb9cc706d7808777d17210
QMuncaster <q.muncaster@gmail.com>
2019-08-08 22:54:46 -0700

    readded first name to header

--- RxOn/imports/ui/layouts/Header.js
+++ RxOn/imports/ui/layouts/Header.js
@@ -6,17 +6,8 @@ import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import ProfileMenu from './ProfileMenu';
-// import { withTracker } from 'meteor/react-meteor-data';
-// import { Tracker } from 'meteor/tracker';
+import { withTracker } from 'meteor/react-meteor-data';
 import { withRouter } from 'react-router-dom';
-
-// reactive variable that we can override for logging out
-// if just use this.props.currentUser, cannot set property to false before waiting for server response
-// so the NavBar would take a couple seconds to disappear after logging out
-// let loggedIn = !!Meteor.user();
-// Tracker.autorun(() => {
-//     loggedIn = !!Meteor.user();
-// });
 
 const styles = {
     grow: {
@@ -31,7 +22,6 @@ class Header extends Component {
 
     render() {
         const { classes } = this.props;
-        //if (loggedIn) {
             return (
                 <div className={classes.grow}>
                     <AppBar position="static">
@@ -40,24 +30,22 @@ class Header extends Component {
                                 <MenuIcon />
                             </IconButton>
                             <Typography variant="h5" color="inherit" className={classes.grow}>
-                                Welcome
+                                {this.props.firstName? "Welcome " + this.props.firstName : "Welcome"}
                             </Typography>
                             <ProfileMenu />
                         </Toolbar>
                     </AppBar>
                 </div>
             );
-        // } else {
-        //     return null;
-        // }
     }
 }
 
 const StyledHeader = withStyles(styles)(Header);
-export default withRouter(StyledHeader);
-// export default withTracker(() => {
-//     Meteor.subscribe('userData');
-//     return {
-//         currentUser: Meteor.user(),
-//     };
-// })(withRouter(StyledHeader));
+// export default withRouter(StyledHeader);
+export default withTracker(() => {
+    Meteor.subscribe('userData');
+    let currentUser = Meteor.users.findOne();
+    return {
+        firstName: currentUser? currentUser.firstname : ' ',
+    };
+})(withRouter(StyledHeader));
