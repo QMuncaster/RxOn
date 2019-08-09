f3ea6df6ba687dd38e14884aec0bec7f2a2ef3b9
omarkhs <30681552+omarkhs@users.noreply.github.com>
2019-08-09 00:51:20 -0700

    remove unused import statement

--- RxOn/imports/ui/patient_components/add_medication_stepper/MedicationInput.js
+++ RxOn/imports/ui/patient_components/add_medication_stepper/MedicationInput.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
 
 export default class MedicationInput extends Component {
     constructor(props) {
