784e45964060227a83bf1c2b7a55b54d28ab4fdb
omarkhs <30681552+omarkhs@users.noreply.github.com>
2019-08-09 00:21:41 -0700

    fix patient list name location

--- RxOn/imports/ui/pharmacist_components/PatientList.js
+++ RxOn/imports/ui/pharmacist_components/PatientList.js
@@ -3,12 +3,12 @@ import { withTracker } from 'meteor/react-meteor-data';
 import IndividualPatient from './IndividualPatient';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import TableRow from "@material-ui/core/TableRow";
-import TableCell from "@material-ui/core/TableCell";
-import TableHead from "@material-ui/core/TableHead";
-import Paper from "@material-ui/core/Paper";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
 
 const styles = theme => ({
     root: {
@@ -23,62 +23,55 @@ const styles = theme => ({
 });
 
 class PatientList extends Component {
-
     constructor(props) {
         super(props);
     }
 
     renderPatients() {
-        return this.props.users.map((user) => {
+        return this.props.users.map(user => {
             //only show users who are patients, not pharamcies
             // (THIS IS ALSO ENSURED ON SERVER SIDE)
             if (user.roles != 'admin') {
-                return <IndividualPatient key={user._id} user={user} />
+                return <IndividualPatient key={user._id} user={user} />;
             }
-        }
-        );
+        });
     }
 
     render() {
         const { classes } = this.props;
         return (
-
-            <div style={{ paddingLeft: 150, paddingRight: 150, paddingTop: 75 }}>
+            <div style={{ paddingLeft: 150, paddingRight: 150, paddingTop: 28 }}>
                 <ul>
-                    <Typography variant="h5">
-                        Patient List
-                </Typography>
                     <Paper className={classes.root}>
-                        <Table className={classes.table} style={{ tableLayout: "auto" }}>
+                        <Typography variant="h5">Patient List</Typography>
+                        <Table className={classes.table} style={{ tableLayout: 'auto' }}>
                             <TableHead>
                                 <TableRow>
-                                    <TableCell align="left" style={{ width: "25%" }}>Patient
-                                                    Name</TableCell>
+                                    <TableCell align="left" style={{ width: '25%' }}>
+                                        Patient Name
+                                    </TableCell>
                                     <TableCell align="center">Age</TableCell>
                                     <TableCell align="center">Gender</TableCell>
                                     <TableCell align="center">History</TableCell>
                                 </TableRow>
                             </TableHead>
-                            <TableBody>
-                                {this.renderPatients()}
-                            </TableBody>
+                            <TableBody>{this.renderPatients()}</TableBody>
                         </Table>
                     </Paper>
                 </ul>
             </div>
-
         );
     }
 }
-
 
 const styledComponent = withStyles(styles)(PatientList);
 export default withTracker(() => {
     Meteor.subscribe('userList');
 
     return {
-        users: Meteor.users.find({}, {
-            sort: { lastname: 1 }
-        }).fetch()
+        users: Meteor.users.find({},
+            {
+                sort: { lastname: 1 },
+            }).fetch(),
     };
 })(styledComponent);
