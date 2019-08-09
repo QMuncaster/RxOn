322f059302caf7a51a93de9f0a393b1acb68ae2c
omarkhs <30681552+omarkhs@users.noreply.github.com>
2019-08-09 00:22:09 -0700

    change view prescriptions icon

--- RxOn/imports/ui/pharmacist_components/ViewMedications.js
+++ RxOn/imports/ui/pharmacist_components/ViewMedications.js
@@ -1,15 +1,23 @@
 import React, { useState } from 'react';
+import { loadCSS } from 'fg-loadcss';
+import clsx from 'clsx';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import Medication from "../patient_components/Medication";
-import IconButton from "@material-ui/core/IconButton";
-import ViewIcon from '@material-ui/icons/ViewList';
+import Medication from '../patient_components/Medication';
+import IconButton from '@material-ui/core/IconButton';
 
 export default function ViewMedicationsDialog(props) {
     const [open, setOpen] = useState(false);
+
+    React.useEffect(() => {
+        loadCSS(
+            'https://use.fontawesome.com/releases/v5.1.0/css/all.css',
+            document.querySelector('#font-awesome-css')
+        );
+    }, []);
 
     function handleClickOpen() {
         setOpen(true);
@@ -21,22 +29,29 @@ export default function ViewMedicationsDialog(props) {
 
     function renderMedication() {
         if (props.prescriptions && props.prescriptions.length > 0) {
-
-            return props.prescriptions.map(px => <Medication key={px._id} ContainerProps={px} hideActions={true}/>);
+            return props.prescriptions.map(px => (
+                <Medication key={px._id} ContainerProps={px} hideActions={true} />
+            ));
         }
         return <div> Oops, no prescriptions available! </div>;
     }
 
     return (
         <React.Fragment>
-            <IconButton variant="outlined" color="primary" onClick={handleClickOpen}>
-                <ViewIcon />
-            </IconButton>
-            <Dialog open={open} onClose={handleClose} aria-labelledby="add-dialog-title" maxWidth="md">
+            <IconButton
+                className={clsx('fas fa-file-medical')}
+                variant="outlined"
+                color="primary"
+                onClick={handleClickOpen}
+            />
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="add-dialog-title"
+                maxWidth="md"
+            >
                 <DialogTitle id="edit-dialog-title">Prescriptions</DialogTitle>
-                <DialogContent>
-                    {renderMedication()}
-                </DialogContent>
+                <DialogContent>{renderMedication()}</DialogContent>
                 <DialogActions>
                     <Button onClick={handleClose} color="primary">
                         Ok
